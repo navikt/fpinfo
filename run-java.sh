99f9a04fe19469c8ebe8cac168ec21225c77e3d0
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-01-16 09:52:16 +0100

    typo

--- run-java.sh
+++ run-java.sh
@@ -4,4 +4,4 @@ set -eu
 export STARTUP_CLASS=${STARTUP_CLASS:-"no.nav.foreldrepenger.info.web.server.JettyServer"}
 export CLASSPATH=app.jar:lib/*
 
-exec java -cp ${CLASSPATH:-"app.jar:lib/*"}  {DEFAULT_JVM_OPTS}  ${JAVA_OPTS}  -Dwebapp=${WEBAPP:-"./webapp"} -Dapplication.name=FPINFO ${STARTUP_CLASS?} ${RUNTIME_OPTS} ${SERVER_PORT:-8080} $@
+exec java -cp ${CLASSPATH:-"app.jar:lib/*"}  {DEFAULT_JVM_OPTS}  ${JAVA_OPTS}  -Dwebapp=${WEBAPP:-"./webapp"} -Dapplication.name=FPINFO ${STARTUP_CLASS?} ${SERVER_PORT:-8080} $@
