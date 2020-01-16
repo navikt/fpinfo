37e546f22cc5ed02c004ca6797d8009eaa5e513d
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-01-16 08:51:34 +0100

    Update run-java.sh

--- run-java.sh
+++ run-java.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env sh
 set -eu
 
-hostname=$(hostname)
-
 export JAVA_OPTS="${JAVA_OPTS:-} -Djava.security.egd=file:/dev/./urandom"
-
-# hvor skal gc log, heap dump etc kunne skrives til med Docker?
-export todo_JAVA_OPTS="${JAVA_OPTS} -XX:ErrorFile=./hs_err_pid<pid>.log -XX:HeapDumpPath=./java_pid<pid>.hprof -XX:-HeapDumpOnOutOfMemoryError -Xloggc:<filename>"
 
 export STARTUP_CLASS=${STARTUP_CLASS:-"no.nav.foreldrepenger.info.web.server.JettyServer"}
 export CLASSPATH=app.jar:lib/*
