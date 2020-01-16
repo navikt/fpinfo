eb3a5742f68d3d694e946d63cf57c04bc27485db
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-01-16 10:06:49 +0100

    revert

--- run-java.sh
+++ run-java.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env sh
-set -eu
+export JAVA_OPTS="${JAVA_OPTS:-} -Djava.security.egd=file:/dev/./urandom"
 
 export STARTUP_CLASS=${STARTUP_CLASS:-"no.nav.foreldrepenger.info.web.server.JettyServer"}
 export CLASSPATH=app.jar:lib/*
+export LOGBACK_CONFIG=${LOGBACK_CONFIG:-"./conf/logback.xml"}
 
-exec java -cp ${CLASSPATH:-"app.jar:lib/*"}  {DEFAULT_JVM_OPTS}  ${JAVA_OPTS}  -Dwebapp=${WEBAPP:-"./webapp"} -Dapplication.name=FPINFO ${STARTUP_CLASS?} ${SERVER_PORT:-8080} $@
+exec java -cp ${CLASSPATH:-"app.jar:lib/*"} ${DEFAULT_JAVA_OPTS:-} ${JAVA_OPTS} -Dlogback.configurationFile=${LOGBACK_CONFIG?} -Dwebapp=${WEBAPP:-"./webapp"} -Dapplication.name=FPINFO ${STARTUP_CLASS?} ${SERVER_PORT:-8080} $@
