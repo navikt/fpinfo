9074b80d8c075954e10c2c3cf13d3b5a98f90e2a
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-01-16 10:17:13 +0100

    logback is in classpath

--- run-java.sh
+++ run-java.sh
@@ -2,6 +2,5 @@ export JAVA_OPTS="${JAVA_OPTS:-} -Djava.security.egd=file:/dev/./urandom"
 
 export STARTUP_CLASS=${STARTUP_CLASS:-"no.nav.foreldrepenger.info.web.server.JettyServer"}
 export CLASSPATH=app.jar:lib/*
-export LOGBACK_CONFIG=${LOGBACK_CONFIG:-"./conf/logback.xml"}
 
-exec java -cp ${CLASSPATH:-"app.jar:lib/*"} ${DEFAULT_JAVA_OPTS:-} ${JAVA_OPTS} -Dlogback.configurationFile=${LOGBACK_CONFIG?} -Dwebapp=${WEBAPP:-"./webapp"} -Dapplication.name=FPINFO ${STARTUP_CLASS?} ${SERVER_PORT:-8080} $@
+exec java -cp ${CLASSPATH:-"app.jar:lib/*"} ${DEFAULT_JAVA_OPTS:-} ${JAVA_OPTS} -Dwebapp=${WEBAPP:-"./webapp"} -Dapplication.name=FPINFO ${STARTUP_CLASS?} ${SERVER_PORT:-8080} $@
