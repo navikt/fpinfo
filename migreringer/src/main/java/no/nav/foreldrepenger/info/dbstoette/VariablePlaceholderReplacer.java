b4ae6965bdaa26c27c036936f4887e9828926ac4
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-01-25 19:48:42 +0100

    simplify

--- migreringer/src/main/java/no/nav/foreldrepenger/info/dbstoette/VariablePlaceholderReplacer.java
+++ migreringer/src/main/java/no/nav/foreldrepenger/info/dbstoette/VariablePlaceholderReplacer.java
@@ -1,11 +1,5 @@
 package no.nav.foreldrepenger.info.dbstoette;
 
-import java.util.Optional;
-import java.util.Properties;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import no.nav.vedtak.util.env.Environment;
 
 public class VariablePlaceholderReplacer {
@@ -13,25 +7,9 @@ public class VariablePlaceholderReplacer {
     private static final String PLACEHOLDER_PREFIX = "${";
     private static final String PLACEHOLDER_SUFFIX = "}";
     private static final Environment ENV = Environment.current();
-    private static final Logger LOG = LoggerFactory.getLogger(VariablePlaceholderReplacer.class);
-    private final Properties properties;
-
-    public VariablePlaceholderReplacer() {
-        this(new Properties());
-    }
-
-    @Deprecated
-    public VariablePlaceholderReplacer(Properties properties) {
-        this.properties = properties;
-        if (!properties.isEmpty()) {
-            LOG.warn(
-                    "Ikke konstruer denne klassen med properties, bruk heller en av application.properties-variantene");
-        }
-    }
 
     public String replacePlaceholders(String input) {
         var key = input.replace(PLACEHOLDER_PREFIX, "").replace(PLACEHOLDER_SUFFIX, "");
-        return key != input ? Optional.ofNullable(ENV.getProperty(key))
-                .orElse(properties.getProperty(key)) : input;
+        return key != input ? ENV.getRequiredProperty(key) : input;
     }
 }
--- migreringer/src/test/java/no/nav/foreldrepenger/info/dbstoette/VariablePlaceholderReplacerTest.java
+++ migreringer/src/test/java/no/nav/foreldrepenger/info/dbstoette/VariablePlaceholderReplacerTest.java
@@ -2,8 +2,6 @@ package no.nav.foreldrepenger.info.dbstoette;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.Properties;
-
 import org.junit.Test;
 
 public class VariablePlaceholderReplacerTest {
@@ -11,12 +9,10 @@ public class VariablePlaceholderReplacerTest {
     @Test
     public void test() {
 
-        Properties properties = new Properties();
-        properties.setProperty("flyway.placeholders.fpinfo.fpsak.schema.navn", "fpsak");
-        properties.setProperty("flyway.placeholders.fpinfoschema.schema.navn", "fpinfo_schema");
-        properties.setProperty("flyway.placeholders.fpinfo.schema.navn", "fpinfo");
-
-        VariablePlaceholderReplacer vpr = new VariablePlaceholderReplacer(properties);
+        System.setProperty("flyway.placeholders.fpinfo.fpsak.schema.navn", "fpsak");
+        System.setProperty("flyway.placeholders.fpinfoschema.schema.navn", "fpinfo_schema");
+        System.setProperty("flyway.placeholders.fpinfo.schema.navn", "fpinfo");
+        VariablePlaceholderReplacer vpr = new VariablePlaceholderReplacer();
         assertEquals(vpr.replacePlaceholders("${flyway.placeholders.fpinfo.schema.navn}"), "fpinfo");
         assertEquals(vpr.replacePlaceholders("${flyway.placeholders.fpinfoschema.schema.navn}"), "fpinfo_schema");
         assertEquals(vpr.replacePlaceholders("${flyway.placeholders.fpinfo.fpsak.schema.navn}"), "fpsak");
