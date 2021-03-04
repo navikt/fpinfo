4e076bf293a77b1b20a46725efc2bcb4990be920
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-03-04 10:15:30 +0100

    registrerer jaxrs filter og test container filter

---
+++ info-web/src/main/java/no/nav/vedtak/sikkerhet/abac/ValidatorFilter.java
@@ -0,0 +1,29 @@
+package no.nav.vedtak.sikkerhet.abac;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import no.nav.security.token.support.jaxrs.JaxrsTokenValidationContextHolder;
+
+public class ValidatorFilter implements ContainerRequestFilter {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ValidatorFilter.class);
+
+    public ValidatorFilter() {
+        LOG.info("Konstruert");
+    }
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) {
+        LOG.info("Request");
+        var token = JaxrsTokenValidationContextHolder.getHolder().getTokenValidationContext().getFirstValidToken();
+        if (token.isPresent()) {
+            LOG.info("Vi har et token");
+        } else {
+            LOG.info("Vi har ikke token");
+        }
+    }
+}
