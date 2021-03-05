ecc89b7187598aed6b413705d6e3a4f751b94004
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-03-05 18:41:34 +0100

    log

--- info-web/src/main/java/no/nav/foreldrepenger/info/web/server/sikkerhet/TokenSupportTokenProvider.java
+++ info-web/src/main/java/no/nav/foreldrepenger/info/web/server/sikkerhet/TokenSupportTokenProvider.java
@@ -3,6 +3,9 @@ package no.nav.foreldrepenger.info.web.server.sikkerhet;
 import javax.annotation.Priority;
 import javax.enterprise.context.Dependent;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import no.nav.security.token.support.core.jwt.JwtToken;
 import no.nav.security.token.support.jaxrs.JaxrsTokenValidationContextHolder;
 import no.nav.vedtak.sikkerhet.abac.TokenProvider;
@@ -11,8 +14,11 @@ import no.nav.vedtak.sikkerhet.abac.TokenProvider;
 @Priority(1)
 public class TokenSupportTokenProvider implements TokenProvider {
 
+    private static final Logger LOG = LoggerFactory.getLogger(TokenSupportTokenProvider.class);
+
     @Override
     public String getUid() {
+        LOG.info("Henter ut uid");
         var token = JaxrsTokenValidationContextHolder.getHolder().getTokenValidationContext().getFirstValidToken();
         return token.map(JwtToken::getSubject).orElseThrow();
 
@@ -20,6 +26,7 @@ public class TokenSupportTokenProvider implements TokenProvider {
 
     @Override
     public String userToken() {
+        LOG.info("Henter ut token");
         var token = JaxrsTokenValidationContextHolder.getHolder().getTokenValidationContext().getFirstValidToken();
         return token.map(JwtToken::getTokenAsString).orElseThrow();
     }
