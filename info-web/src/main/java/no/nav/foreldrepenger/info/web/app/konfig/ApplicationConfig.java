bca190a57b0f43f640a2dfb05a976754358dbcaf
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-03-16 13:22:21 +0100

    flyttet swagger

--- info-web/src/main/java/no/nav/foreldrepenger/info/web/app/konfig/ApplicationConfig.java
+++ info-web/src/main/java/no/nav/foreldrepenger/info/web/app/konfig/ApplicationConfig.java
@@ -7,12 +7,6 @@ import javax.ws.rs.core.Application;
 
 import io.swagger.v3.jaxrs2.SwaggerSerializers;
 import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
-import io.swagger.v3.oas.integration.GenericOpenApiContextBuilder;
-import io.swagger.v3.oas.integration.OpenApiConfigurationException;
-import io.swagger.v3.oas.integration.SwaggerConfiguration;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
 import no.nav.foreldrepenger.info.web.app.exceptions.ConstraintViolationMapper;
 import no.nav.foreldrepenger.info.web.app.exceptions.GeneralRestExceptionMapper;
 import no.nav.foreldrepenger.info.web.app.exceptions.JsonMappingExceptionMapper;
@@ -25,25 +19,6 @@ import no.nav.security.token.support.jaxrs.JwtTokenContainerRequestFilter;
 public class ApplicationConfig extends Application {
 
     static final String API_URI = "/api";
-
-    public ApplicationConfig() throws OpenApiConfigurationException {
-
-        new GenericOpenApiContextBuilder<>()
-                .openApiConfiguration(new SwaggerConfiguration()
-                        .openAPI(new OpenAPI()
-                                .info(new Info()
-                                        .title("Vedtaksløsningen - Info")
-                                        .version("1.0")
-                                        .description("REST grensesnitt for Vedtaksløsningen."))
-                                .addServersItem(new Server()
-                                        .url("/fpinfo" + API_URI)))
-                        .prettyPrint(true)
-                        .scannerClass("io.swagger.v3.jaxrs2.integration.JaxrsAnnotationScanner")
-                        .resourcePackages(Set.of("no.nav")))
-                .buildContext(true)
-                .read();
-
-    }
 
     @Override
     public Set<Class<?>> getClasses() {
