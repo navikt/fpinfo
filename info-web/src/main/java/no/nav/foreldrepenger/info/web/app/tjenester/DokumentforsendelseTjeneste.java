a5147661b13cc1bc736e5cb2298bddb8a7d56eb7
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-03-23 13:24:36 +0100

    log token i dev

--- info-web/src/main/java/no/nav/foreldrepenger/info/web/app/tjenester/DokumentforsendelseTjeneste.java
+++ info-web/src/main/java/no/nav/foreldrepenger/info/web/app/tjenester/DokumentforsendelseTjeneste.java
@@ -16,6 +16,7 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.swagger.v3.oas.annotations.Operation;
