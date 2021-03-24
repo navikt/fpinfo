62d88491f1cb7671f06c9a00b409cb87cef9db49
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-03-24 16:52:07 +0100

    log filter

---
+++ info-web/src/main/java/no/nav/foreldrepenger/info/web/server/LoggingFilter.java
@@ -0,0 +1,35 @@
+package no.nav.foreldrepenger.info.web.server;
+
+import java.io.IOException;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@WebFilter(dispatcherTypes = DispatcherType.REQUEST, urlPatterns = "/api/*")
+public class LoggingFilter implements Filter {
+    private static final Logger LOG = LoggerFactory.getLogger(LoggingFilter.class);
+
+    public LoggingFilter() {
+        LOG.info("CONSTRUCT");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        LOG.info("DOFILTER");
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+        LOG.info("INIT");
+    }
+
+}
