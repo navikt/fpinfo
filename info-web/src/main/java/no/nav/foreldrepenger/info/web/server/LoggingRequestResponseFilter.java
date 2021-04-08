64515ce07979844a586b1e954db5ebad27419b0f
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-04-08 10:29:25 +0200

    log

--- info-web/src/main/java/no/nav/foreldrepenger/info/web/server/LoggingRequestResponseFilter.java
+++ info-web/src/main/java/no/nav/foreldrepenger/info/web/server/LoggingRequestResponseFilter.java
@@ -2,6 +2,7 @@ package no.nav.foreldrepenger.info.web.server;
 
 import java.io.IOException;
 
+import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ContainerResponseContext;
@@ -11,24 +12,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 //@Provider
+@RequestScoped
 public class LoggingRequestResponseFilter implements ContainerRequestFilter, ContainerResponseFilter {
     private static final Logger LOG = LoggerFactory.getLogger(LoggingRequestResponseFilter.class);
     int i;
 
     public LoggingRequestResponseFilter() {
-        LOG.info("FILTER CONSTRUCT {}", this);
+        LOG.info("FILTER CONSTRUCT {}", this.hashCode());
     }
 
     @Override
     public void filter(ContainerRequestContext containerRequestContext) {
-        LOG.info("FILTER REQUEST {} {}", this, i);
+        LOG.info("FILTER REQUEST {} {}", this.hashCode(), i);
         i++;
 
     }
 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
-        LOG.info("FILTER RESPONSE {} {}", this, i);
+        LOG.info("FILTER RESPONSE {} {}", this.hashCode(), i);
         i++;
     }
 }
