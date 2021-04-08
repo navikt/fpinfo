382b9a9ceb124c6fd4d7bc9030d154a712b99b1d
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-04-08 15:54:19 +0200

    inject some stuff

--- info-web/src/main/java/no/nav/foreldrepenger/info/web/server/LoggingRequestResponseFilter.java
+++ info-web/src/main/java/no/nav/foreldrepenger/info/web/server/LoggingRequestResponseFilter.java
@@ -3,6 +3,7 @@ package no.nav.foreldrepenger.info.web.server;
 import java.io.IOException;
 
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ContainerResponseContext;
@@ -11,28 +12,50 @@ import javax.ws.rs.container.ContainerResponseFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import no.nav.vedtak.sikkerhet.abac.AbacAuditlogger;
+import no.nav.vedtak.sikkerhet.abac.AbacSporingslogg;
+import no.nav.vedtak.sikkerhet.abac.Pep;
+import no.nav.vedtak.sikkerhet.abac.TokenProvider;
+import no.nav.vedtak.util.env.Environment;
+
 @RequestScoped
 public class LoggingRequestResponseFilter implements ContainerRequestFilter, ContainerResponseFilter {
     private static final Logger LOG = LoggerFactory.getLogger(LoggingRequestResponseFilter.class);
     int i;
 
-    // @Context
-    // private UriInfo info;
+    private final Pep pep;
+    private final AbacSporingslogg sporingslogg;
+    private final AbacAuditlogger abacAuditlogger;
+    private static final Environment ENV = Environment.current();
+    private final TokenProvider tokenProvider;
 
-    public LoggingRequestResponseFilter() {
+    @Inject
+    public LoggingRequestResponseFilter(Pep pep,
+            AbacSporingslogg sporingslogg, AbacAuditlogger abacAuditlogger,
+            TokenProvider tokenProvider) {
+        this.pep = pep;
+        this.sporingslogg = sporingslogg;
+        this.abacAuditlogger = abacAuditlogger;
+        this.tokenProvider = tokenProvider;
         LOG.info("FILTER CONSTRUCT {}", this);
     }
 
     @Override
     public void filter(ContainerRequestContext containerRequestContext) {
-        LOG.info("FILTER REQUEST {} {}", this.hashCode(), i);
+        LOG.info("FILTER REQUEST {} {}", this, i);
         i++;
 
     }
 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
-        LOG.info("FILTER RESPONSE {} {}", this.hashCode(), i);
+        LOG.info("FILTER RESPONSE {} {}", this, i);
         i++;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + " [pep=" + pep + ", sporingslogg=" + sporingslogg + ", abacAuditlogger=" + abacAuditlogger
+                + ", tokenProvider=" + tokenProvider + "]";
     }
 }
