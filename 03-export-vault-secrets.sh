ec73147a5f05174bccdbe52e12962cf76f9052e0
Anders Palfi <anderspalfi@gmail.com>
2021-10-18 10:43:36 +0200

    Forsøk på å sjekke om en datasource holder

--- 03-export-vault-secrets.sh
+++ 03-export-vault-secrets.sh
@@ -1,37 +1,16 @@
 #!/usr/bin/env bash
 
-if test -f /var/run/secrets/nais.io/defaultDS/username;
-then
-   export  DEFAULTDS_USERNAME=$(cat /var/run/secrets/nais.io/defaultDS/username)
-   echo "Setting DEFAULTDS_USERNAME to $DEFAULTDS_USERNAME"
-    
-fi
-
-if test -f /var/run/secrets/nais.io/defaultDS/password;
-then
-    export  DEFAULTDS_PASSWORD=$(cat /var/run/secrets/nais.io/defaultDS/password)
-    echo "Setting DEFAULTDS_PASSWORD"
-fi
-
-
 if test -f /var/run/secrets/nais.io/fpinfoSchema/username;
 then
    export  FPINFOSCHEMA_USERNAME=$(cat /var/run/secrets/nais.io/fpinfoSchema/username)
    echo "Setting FPINFOSCHEMA_USERNAME to $FPINFOSCHEMA_USERNAME"
-    
+
 fi
 
 if test -f /var/run/secrets/nais.io/fpinfoSchema/password;
 then
     export  FPINFOSCHEMA_PASSWORD=$(cat /var/run/secrets/nais.io/fpinfoSchema/password)
     echo "Setting FPINFOSCHEMA_PASSWORD"
-fi
-
-
-if test -f /var/run/secrets/nais.io/defaultDSconfig/jdbc_url;
-then
-    export  DEFAULTDS_URL=$(cat /var/run/secrets/nais.io/defaultDSconfig/jdbc_url)
-    echo "Setting DEFAULTDS_URL til $DEFAULTDS_URL"
 fi
 
 if test -f /var/run/secrets/nais.io/fpinfoSchemaDSconfig/jdbc_url;
--- info-migreringer/src/test/java/no/nav/foreldrepenger/info/dbstoette/VariablePlaceholderReplacerTest.java
+++ info-migreringer/src/test/java/no/nav/foreldrepenger/info/dbstoette/VariablePlaceholderReplacerTest.java
@@ -9,12 +9,8 @@ class VariablePlaceholderReplacerTest {
 
     @Test
     void test() {
-        System.setProperty("flyway.placeholders.fpinfo.fpsak.schema.navn", "fpsak");
         System.setProperty("flyway.placeholders.fpinfoschema.schema.navn", "fpinfo_schema");
-        System.setProperty("flyway.placeholders.fpinfo.schema.navn", "fpinfo");
-        assertThat(VariablePlaceholderReplacer.replacePlaceholders("${flyway.placeholders.fpinfo.schema.navn}")).isEqualTo("fpinfo");
         assertThat(VariablePlaceholderReplacer.replacePlaceholders("${flyway.placeholders.fpinfoschema.schema.navn}")).isEqualTo("fpinfo_schema");
-        assertThat(VariablePlaceholderReplacer.replacePlaceholders("${flyway.placeholders.fpinfo.fpsak.schema.navn}")).isEqualTo("fpsak");
         assertThat(VariablePlaceholderReplacer.replacePlaceholders("ingen")).isEqualTo("ingen");
         //application-local.properties
         assertThat(VariablePlaceholderReplacer.replacePlaceholders("${flyway.test}")).isEqualTo("hallo");
