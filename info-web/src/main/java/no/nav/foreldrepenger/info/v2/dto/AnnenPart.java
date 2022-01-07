9d1e6dd0004646ac729a3c7bb41582546fe5b813
Steffen Pøhner <steffen.h.pohner@nav.no>
2022-01-07 12:59:16 +0100

    SakerV2 fix - unwrap AnnenPart

--- info-web/src/main/java/no/nav/foreldrepenger/info/v2/dto/AnnenPart.java
+++ info-web/src/main/java/no/nav/foreldrepenger/info/v2/dto/AnnenPart.java
@@ -3,12 +3,19 @@ package no.nav.foreldrepenger.info.v2.dto;
 import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 
 
 public record AnnenPart(PersonDetaljer personDetaljer) {
 
-    @JsonCreator
+    @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
     public AnnenPart {
         Objects.requireNonNull(personDetaljer, "Persondetaljer kan ikke være null");
+    }
+
+    @Override
+    @JsonValue
+    public PersonDetaljer personDetaljer() {
+        return personDetaljer;
     }
 }
