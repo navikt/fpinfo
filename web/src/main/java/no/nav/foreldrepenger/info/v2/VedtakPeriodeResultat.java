3727d0a5afd7424dc57f59f2e75a06442f18a929
Anders Palfi <anderspalfi@gmail.com>
2022-09-02 11:23:20 +0200

    TFP-5168 saksmodell utvider vedtaksperioder med om det trekkes fra minsterett (#1399)

--- web/src/main/java/no/nav/foreldrepenger/info/v2/VedtakPeriodeResultat.java
+++ web/src/main/java/no/nav/foreldrepenger/info/v2/VedtakPeriodeResultat.java
@@ -1,7 +1,7 @@
 package no.nav.foreldrepenger.info.v2;
 
-record VedtakPeriodeResultat(boolean innvilget) {
+record VedtakPeriodeResultat(boolean innvilget, boolean trekkerMinsterett) {
     no.nav.foreldrepenger.common.innsyn.v2.VedtakPeriodeResultat tilDto() {
-        return new no.nav.foreldrepenger.common.innsyn.v2.VedtakPeriodeResultat(innvilget);
+        return new no.nav.foreldrepenger.common.innsyn.v2.VedtakPeriodeResultat(innvilget, trekkerMinsterett);
     }
 }
