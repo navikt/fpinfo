d4f768339c7dda26a57f85822dbe5228da50d861
Anders Palfi <anderspalfi@gmail.com>
2023-02-03 10:14:32 +0100

    Sammenligner ny og gammel måte å hente familiehendelser (#1569)

--- web/src/main/java/no/nav/foreldrepenger/info/v2/FpSakerTjeneste.java
+++ web/src/main/java/no/nav/foreldrepenger/info/v2/FpSakerTjeneste.java
@@ -86,8 +86,7 @@ class FpSakerTjeneste {
         var søknadsgrunnlag = søknadsgrunnlagOpt.get();
         var tilhørerMor = tilhørerSakMor(søknadsgrunnlag);
         var annenPart = annenPart(fpSak).orElse(null);
-        //TODO erstatte med nytt familiehendelse view. Likt svp
-        var familiehendelse = familiehendelse(søknadsgrunnlag);
+        var familiehendelse = familiehendelse(søknadsgrunnlag, åpenBehandling.getBehandlingId());
 
         var barn = barn(fpSak.saksnummer());
         var rettighetType = rettighetTypeFraSøknad(søknadsgrunnlag);
@@ -125,7 +124,7 @@ class FpSakerTjeneste {
         var søknadsgrunnlag = finnSøknadsgrunnlag(gjeldendeVedtakBehandlingId)
                 .orElseThrow(() -> new IllegalStateException("Forventer søknadsgrunnlag på behandling"));
         var tilhørerMor = tilhørerSakMor(søknadsgrunnlag);
-        var familiehendelse = familiehendelse(søknadsgrunnlag);
+        var familiehendelse = familiehendelse(søknadsgrunnlag, gjeldendeVedtakBehandlingId);
         var vedtaksperioder = hentVedtakPerioder(gjeldendeVedtakBehandlingId);
         var gjeldendeVedtak = new FpVedtak(vedtaksperioder);
         var sakAvsluttet = Objects.equals(fpSak.fagsakStatus(), "AVSLU");
@@ -159,9 +158,20 @@ class FpSakerTjeneste {
         return repository.hentSøknadsGrunnlag(behandlingId);
     }
 
-    private static Familiehendelse familiehendelse(SøknadsGrunnlag søknadsgrunnlag) {
-        return new Familiehendelse(søknadsgrunnlag.getFødselDato(), søknadsgrunnlag.getTermindato(),
+    private Familiehendelse familiehendelse(SøknadsGrunnlag søknadsgrunnlag, Long behandlingId) {
+        //TODO erstatte med nytt familiehendelse view
+        var familiehendelse = new Familiehendelse(søknadsgrunnlag.getFødselDato(), søknadsgrunnlag.getTermindato(),
                 søknadsgrunnlag.getAntallBarn(), søknadsgrunnlag.getOmsorgsovertakelseDato());
+        try {
+            var fh = repository.hentFamilieHendelse(behandlingId).orElseThrow();
+            var familiehendelse2 = new Familiehendelse(fh.getFødselsdato(), fh.getTermindato(), fh.getAntallBarn(), fh.getOmsorgsovertakelseDato());
+            if (!familiehendelse.equals(familiehendelse2)) {
+                LOG.info("Familiehendelser matcher ikke {}", behandlingId);
+            }
+        } catch (Exception e) {
+            LOG.info("Feil ved henting av familiehendelse fra nytt view {}", behandlingId, e);
+        }
+        return familiehendelse;
     }
 
     private static Dekningsgrad dekningsgrad(SøknadsGrunnlag søknadsgrunnlag) {
