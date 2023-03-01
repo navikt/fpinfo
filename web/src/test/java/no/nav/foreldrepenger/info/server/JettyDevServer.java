c42d084729aae3efb589776d8f427b147a1f799e
Anders Palfi <anders.palfi@nav.no>
2023-03-01 12:25:03 +0100

    Fjerner resttjeneste i testscope

--- web/src/test/java/no/nav/foreldrepenger/info/server/JettyDevServer.java
+++ web/src/test/java/no/nav/foreldrepenger/info/server/JettyDevServer.java
@@ -1,14 +1,6 @@
 package no.nav.foreldrepenger.info.server;
 
 
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import javax.ws.rs.ApplicationPath;
-
-import no.nav.foreldrepenger.info.app.konfig.ApiConfig;
-import no.nav.foreldrepenger.info.v2.TestTjeneste;
 import no.nav.foreldrepenger.konfig.Environment;
 
 public class JettyDevServer extends JettyServer {
@@ -28,21 +20,6 @@ public class JettyDevServer extends JettyServer {
 
     private JettyDevServer(int serverPort) {
         super(serverPort);
-    }
-
-    @ApplicationPath(ApiConfig.API_URI)
-    private static class LocalApplicationConfig extends ApiConfig {
-
-        public LocalApplicationConfig() {
-            super();
-        }
-
-        @Override
-        public Set<Class<?>> getClasses() {
-            return Stream.concat(super.getClasses().stream(),
-                            Stream.of(TestTjeneste.class))
-                    .collect(Collectors.toUnmodifiableSet());
-        }
     }
 }
 
