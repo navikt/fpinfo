be8e2f9ab901d94ef879d734d1993885b95112bc
Anders Palfi <anderspalfi@gmail.com>
2023-04-18 09:42:01 +0200

    felles 2.2.0 (#1629)

--- web/src/main/java/no/nav/foreldrepenger/info/v2/AnnenPart.java
+++ web/src/main/java/no/nav/foreldrepenger/info/v2/AnnenPart.java
@@ -9,7 +9,7 @@ record AnnenPart(AktørId aktørId) {
         Objects.requireNonNull(aktørId, "aktørId kan ikke være null");
     }
 
-    no.nav.foreldrepenger.common.innsyn.AnnenPart tilDto() {
-        return new no.nav.foreldrepenger.common.innsyn.AnnenPart(aktørId.tilDto());
+    no.nav.foreldrepenger.common.innsyn.Person tilDto() {
+        return new no.nav.foreldrepenger.common.innsyn.Person(null, new no.nav.foreldrepenger.common.domain.AktørId(aktørId().value()));
     }
 }
